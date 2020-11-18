fbcbe0b661668ddfb07d1834b737db0038e2e0b6
João Bravo <joao_bravo123@hotmail.com>
2020-11-18 17:33:20 +0000

    added templates

---
+++ templates/fit.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <ctime>
+
+#include "TROOT.h"
+#include "TSystem.h"
+#include "TStyle.h"
+#include "TApplication.h"
+#include "TCanvas.h"
+#include "TFrame.h"
+#include "TAxis.h"
+// #include "TGraph.h"
+#include "TGraphErrors.h"
+#include "TF1.h"
+// #include "TFormula.h"
+#include "TMath.h"
+#include "TRandom.h"
+#include "TLine.h"
+#include "TLegend.h"
+
+using namespace std;
+
+
+// Fit Function
+Double_t fitFunc(Double_t *x, Double_t *par)
+{
+	Double_t fitval = par[0]*pow(x[0]-par[1],2) + par[2];
+	return fitval;
+}
+
+
+// Main routine
+int main()
+{
+    // Create TGraphErrors from file
+    string FILE1 = "tools/data/pila.data";
+    vector<TGraphErrors*> DataSaver;
+    DataSaver.push_back(new TGraphErrors(FILE1.c_str(),"%lg %lg %lg %lg","")); // X,Y,eX,eY
+
+    // Canvas
+    TApplication *theApp = new TApplication("App", 0, 0);
+    TCanvas *c1 = new TCanvas("c1", "Fit Solution", 0, 0, 700, 500);
+    c1->SetFillColor(kWhite);
+    c1->SetGrid();
+    c1->GetFrame()->SetFillColor(21);
+    c1->GetFrame()->SetBorderSize(12);
+
+    // Pad
+    TPad *p1 = new TPad("p1", "Pad1", 0, 0, 1, 1, kWhite);
+    // p1->SetLogy();
+    p1->SetGrid();
+    p1->Draw();
+    p1->cd();
+
+    // TGraphErrors
+    DataSaver[0]->SetLineColor(kRed);
+    DataSaver[0]->SetLineWidth(2);
+    DataSaver[0]->SetMarkerStyle(8);
+    DataSaver[0]->SetMarkerColor(0); // kPink
+    DataSaver[0]->SetMarkerSize(2);
+    DataSaver[0]->SetFillColor(0);
+    DataSaver[0]->SetTitle("");
+
+    // Axis
+    DataSaver[0]->GetXaxis()->SetTitle("#tau (ns)");
+    DataSaver[0]->GetYaxis()->SetTitle("K (u.a.)");
+    // DataSaver[0]->GetXaxis()->SetRangeUser(0, 2);
+    // DataSaver[0]->GetYaxis()->SetRangeUser(0, 10);
+
+    // User fit region
+    Double_t lwlim = -100.;
+    Double_t uplim = 1100.;
+    Int_t Npar = 3;
+
+    // Fit Function
+    TF1 *func1 = new TF1("myfit", fitFunc, lwlim, uplim, Npar);
+    func1->SetLineColor(kBlue);
+    func1->SetLineWidth(2);
+
+    // Set initial values and parameter names
+    // func1->SetParameter(0, -1.);
+    // func1->SetParameter(1, 550.);
+    // func1->SetParameter(2, 200.);
+
+    // func1->SetParNames("A","B","C");
+    // func1->SetParLimits(2, -10, -4);
+    // func1->FixParameter(0, 1.186e-1);
+
+    // Draw
+    DataSaver[0]->Draw("AP");
+    DataSaver[0]->Fit("myfit","UR");
+
+    // Legend
+    // TLegend *leg = new TLegend(0.68,0.82,0.88,0.88);
+    // leg->SetFillColor(0);
+    // leg->SetBorderSize(0);
+    // leg->SetTextSize(0.05);
+    // leg->AddEntry(func1,"Func do crl","lp");
+    // leg->Draw();
+
+    // Statistics
+    Double_t chi2 = func1->GetChisquare();
+    Double_t ndf = func1->GetNDF();
+    cout << "\n Chi2: " << chi2 << endl;
+    cout << " ndf: " << ndf << endl;
+    cout << " Chi2/ndf: " << chi2/ndf << endl << endl;
+
+    c1->Modified();
+    c1->Update();
+    while(c1->WaitPrimitive()) gSystem->ProcessEvents();
+    c1->Print("results/fit.pdf");
+
+    delete p1;
+    delete c1;
+}
